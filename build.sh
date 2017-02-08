dd3c209c83d6d132e99a9fceb84e5964e5f19b74
Mingcong Bai <jeffbai@aosc.xyz>
2017-02-07 20:43:28 -0600

    build.sh: fix tarball generation

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ for i in `cat arch-list`; do
     make clean
     make ARCH="$i"
     make install DESTDIR=`pwd`/out/aosc-os-artworks-"$date"-"$i"
+    ( cd out
     tar cvf aosc-os-artworks-"$date"-"$i".tar.xz \
-            `pwd`/out/aosc-os-artworks-"$date"-"$i"
+            aosc-os-artworks-"$date"-"$i"
+    )
 done
